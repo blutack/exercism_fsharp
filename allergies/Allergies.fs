4ad384a4025d319ae0b72c88119bece92c7c2c0c
Gareth R <blutack@gmail.com>
2017-11-01 00:25:06 +0000

    Add Allergies exercise

---
+++ allergies/Allergies.fs
@@ -0,0 +1,22 @@
+﻿module Allergies
+
+open System
+
+type Allergen =
+| Eggs = 0
+| Peanuts = 1
+| Shellfish = 2
+| Strawberries = 3
+| Tomatoes = 4
+| Chocolate = 5
+| Pollen = 6
+| Cats = 7
+
+let allAllergens : Allergen list = 
+    unbox (Enum.GetValues(typeof<Allergen>)) |> Seq.toList
+
+let allergicTo codedAllergies (allergen: Allergen) =
+    codedAllergies &&& (1 <<< int allergen) > 0
+
+let list codedAllergies = 
+    allAllergens |> List.filter (allergicTo codedAllergies) 
